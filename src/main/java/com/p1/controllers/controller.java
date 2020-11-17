7dbd5c36bd71199fbd399b2cd24bf341386cbd4f
pawan <pawanattri.sharma555@gmail.com>
2020-11-17 11:46:05 +0530

    adding welcome method

--- src/main/java/com/p1/controllers/controller.java
+++ src/main/java/com/p1/controllers/controller.java
@@ -1,17 +1,22 @@
 package com.p1.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class controller {
 
 	@ResponseBody
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	@GetMapping(value = "/home")
 	public String home() {
 		return "Welcome to the Spring-Boot Project";
+	}
+	
+	@ResponseBody
+	@GetMapping(value = "/welcome")
+	public String welcome() {
+		
+		return "Welcome to the Spring boot application";
 	}
 }
